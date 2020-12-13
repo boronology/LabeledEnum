8124da2dee45d87105399ee1679926e4864ae97c
boronology <boronology@gmail.com>
2020-12-13 10:54:37 +0900

    引数がnullでもTryParseが例外を投げるのはよくないのでfalseを返すようにする

--- LabeledEnum/EnumLabel.cs
+++ LabeledEnum/EnumLabel.cs
@@ -9,17 +9,17 @@ namespace LabeledEnum
         {
             if (label is null)
             {
-                throw new ArgumentNullException();
+                throw new ArgumentNullException ();
             }
 
-            var valueAndAttribute = GetEnumAndLabel<T>(label);
+            var valueAndAttribute = GetEnumAndLabel<T> (label);
             if (valueAndAttribute.Length == 0)
             {
-                throw new Exception($"ラベル\"{label}\"を持つ値はEnum\"{typeof(T)}\"に存在しません");
+                throw new Exception ($"ラベル\"{label}\"を持つ値はEnum\"{typeof(T)}\"に存在しません");
             }
             if (valueAndAttribute.Length > 1)
             {
-                throw new Exception($"ラベル\"{label}\"を持つ値はEnum\"{typeof(T)}\"に複数存在します（{string.Join(",", valueAndAttribute.Select(x => x.Item1))}）");
+                throw new Exception ($"ラベル\"{label}\"を持つ値はEnum\"{typeof(T)}\"に複数存在します（{string.Join(",", valueAndAttribute.Select(x => x.Item1))}）");
             }
             return valueAndAttribute[0].Item1;
         }
@@ -28,9 +28,10 @@ namespace LabeledEnum
         {
             if (label is null)
             {
-                throw new ArgumentNullException();
+                result = default;
+                return false;
             }
-            var valueAndAttribute = GetEnumAndLabel<T>(label);
+            var valueAndAttribute = GetEnumAndLabel<T> (label);
             if (valueAndAttribute.Length == 1)
             {
                 result = valueAndAttribute[0].Item1;
@@ -41,20 +42,20 @@ namespace LabeledEnum
             return false;
         }
 
-        private static (T, EnumLabelAttribute)[] GetEnumAndLabel<T>(string label) where T : Enum
+        private static (T, EnumLabelAttribute) [] GetEnumAndLabel<T> (string label) where T : Enum
         {
-            var type = typeof(T);
+            var type = typeof (T);
 
-            return Enum.GetValues(type)
-                        .OfType<T>()
-                        .Select(x =>
-                        {
-                            var field = type.GetField(x.ToString());
-                            var attribute = field.GetCustomAttributes(false).OfType<EnumLabelAttribute>().FirstOrDefault();
-                            return (x, attribute);
-                        })
-                        .Where(x => x.attribute?.Label == label)
-                        .ToArray();
+            return Enum.GetValues (type)
+                .OfType<T> ()
+                .Select (x =>
+                {
+                    var field = type.GetField (x.ToString ());
+                    var attribute = field.GetCustomAttributes (false).OfType<EnumLabelAttribute> ().FirstOrDefault ();
+                    return (x, attribute);
+                })
+                .Where (x => x.attribute?.Label == label)
+                .ToArray ();
         }
     }
 }
